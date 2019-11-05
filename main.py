3734f147de39837820131ec4ae99344e5f0c594f
Sadi Kneipp <sadi.kneipp@nubank.com.br>
2019-11-05 00:15:56 -0300

    add support for another argument in predict

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from model import model_load, predict, MODEL
-
+import logging
 from pathlib import Path
 
-import logging
+
+from toolz import merge
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+
+from model import model_load, predict, MODEL
+
 
 logging.basicConfig(
     filename="{}".format(Path.home() / "logs" / "predictor.log"),
@@ -23,11 +27,12 @@ def prediction():
     logging.debug("Received prediction request.")
 
     img = request.files["media"]
-    predicted_pill = predict(model, img.stream)
+    predicted_pill, all_probs = predict(model, img.stream)
 
     logging.debug("Found {}.".format(predicted_pill))
-    return jsonify({"medication": str(predicted_pill).replace("_", " ")})
+    ret_dict = merge({"medication": str(predicted_pill).replace("_", " ")}, {"all_probs": all_probs})
+    return jsonify(ret_dict)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
